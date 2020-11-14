0cff44651e61bed4547d37db16c39681ad3c9981
Pravin Tripathi <pravinyo12@gmail.com>
2020-11-14 15:23:40 +0530

    def added

--- src/behavioral/chain_of_responsibility/Demo.kt
+++ src/behavioral/chain_of_responsibility/Demo.kt
@@ -7,7 +7,13 @@ import behavioral.chain_of_responsibility.server.Server
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-
+/**
+ * Chain of Responsibility is a behavioral design pattern that lets you pass requests
+ * along a chain of handlers. Upon receiving a request, each handler decides either to
+ * process the request or to pass it to the next handler in the chain.
+ *
+ * URL: https://refactoring.guru/design-patterns/chain-of-responsibility
+ */
 object Demo {
 
     private val reader = BufferedReader(InputStreamReader(System.`in`))
