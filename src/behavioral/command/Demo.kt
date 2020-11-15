5a09940d1c58cb7b6091c6f06b8de67782edc77e
Pravin Tripathi <pravinyo12@gmail.com>
2020-11-15 12:26:12 +0530

    def added

--- src/behavioral/command/Demo.kt
+++ src/behavioral/command/Demo.kt
@@ -2,6 +2,14 @@ package behavioral.command
 
 import behavioral.command.editor.Editor
 
+/**
+ * Command is a behavioral design pattern that turns a request into a stand-alone object
+ * that contains all information about the request. This transformation lets you parameterize
+ * methods with different requests, delay or queue a request’s execution, and support undoable
+ * operations.
+ *
+ * URL: https://refactoring.guru/design-patterns/command
+ */
 object Demo {
     @JvmStatic
     fun main(args: Array<String>) {
