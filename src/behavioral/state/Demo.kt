6dfb2d32ff626f4ed80aa493f225dd7d5b832aad
Pravin Tripathi <pravinyo12@gmail.com>
2020-11-20 22:01:13 +0530

    def added

--- src/behavioral/state/Demo.kt
+++ src/behavioral/state/Demo.kt
@@ -3,6 +3,12 @@ package behavioral.state
 import behavioral.state.ui.Player
 import behavioral.state.ui.UI
 
+/**
+ * State is a behavioral design pattern that lets an object alter its behavior
+ * when its internal state changes. It appears as if the object changed its class.
+ *
+ * URL: https://refactoring.guru/design-patterns/state
+ */
 object Demo {
     @JvmStatic
     fun main(args: Array<String>) {
